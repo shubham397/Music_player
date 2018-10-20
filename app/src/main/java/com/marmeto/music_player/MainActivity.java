6b6389783d840db2a594c71c1de583916037cc60
Shubham Kumar <shubhamkumar397@gmail.com>
2018-10-20 08:30:11 +0530

    final

--- app/src/main/java/com/marmeto/music_player/MainActivity.java
+++ app/src/main/java/com/marmeto/music_player/MainActivity.java
@@ -1,13 +1,20 @@
 package com.marmeto.music_player;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
+
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(MainActivity.this,R.raw.awargi);
+
+        mediaPlayer.start();
     }
 }
